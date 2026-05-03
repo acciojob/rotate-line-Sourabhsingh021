dc5d3d310181a28d36864b0569e13d3da2bf0e01
Sourabh Singh <67355597+Sourabhsingh021@users.noreply.github.com>
2026-05-03 11:04:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const line = document.getElementById("line");
+let angle = 0;
+
+setInterval(()=>{
+	angle++;
+	line.style.tranform = `rotate({angle}deg)`;
+} 20);
